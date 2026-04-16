79d083fb03b1171e751433bf7ae2301ad536fd15
Julia <Juliaa@example.com>
2026-04-16 00:31:38 -0700

    ЛАбораторная 8

---
+++ laravel/Illuminate/Support/Facades/DB/Seeds.php
@@ -0,0 +1,27 @@
+<?php
+	use Illuminate\Support\Facades\DB;
+	
+	class DatabaseSeeder extends Seeder
+	{
+		public function run()
+		{
+            DB::table('posts')->insert([
+				[
+					'title' => 'title 1',
+					'slug'  => 'post-1',
+					'text'  => 'text text text 1',
+				],
+				[
+					'title' => 'title 2',
+					'slug'  => 'post-2',
+					'text'  => 'text text text 2',
+				],
+				[
+					'title' => 'title 3',
+					'slug'  => 'post-3',
+					'text'  => 'text text text 3',
+				],
+			]);
+		}
+	}
+?>
